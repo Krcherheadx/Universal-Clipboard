001c4af4cbcce81a02ab6147719dbc6abf7c958d
Hisham AlSuhaibani <H.AlSuhaibani@Futurework.com.sa>
2025-07-10 16:30:42 +0300

    preparing for spring JPA

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE if not exists "User"
+-- (
+--     Id   varchar(20) primary key,
+--     name varchar(20)
+-- );
+CREATE TABLE IF NOT EXISTS "TESTTT"
+(
+    ID serial primary key
+);
